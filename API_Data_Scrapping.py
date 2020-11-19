fb8e65886b6ba1f2f087712eea42959f9992b190
Sadanand Prajapati <30380750+Sadanand-Prajapati@users.noreply.github.com>
2020-11-19 12:24:16 +0530

    Update API_Data_Scrapping.py

--- API_Data_Scrapping.py
+++ API_Data_Scrapping.py
@@ -16,7 +16,7 @@ import pymysql
 from sqlalchemy import create_engine, Column, Integer, String
 import MySQLdb
 
-
+#Get API data class.
 class get_api_data:
     
     def __init__(self,date,zip_code,line_up_id,date_time):
@@ -25,7 +25,6 @@ class get_api_data:
         self.date_time = date_time
         self.line_up_id = line_up_id
     
-
     def get_movies_playing_in_local_theaters(self):
         
         url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate={date}&zip={zip_code}&api_key={api_key}'.\
@@ -36,7 +35,6 @@ class get_api_data:
         df = pd.DataFrame.from_dict(data)
         
         return df
-        
         
     def get_movies_airing_on_TV(self):
         
@@ -49,10 +47,9 @@ class get_api_data:
         
         return df
 
-
+#Creating data models
 
 Base = declarative_base()
-
 class create_table_theater_movies(Base):
         
     __tablename__ = 'theater_movies'
@@ -75,23 +72,27 @@ class create_table_TV_movies(Base):
     description = Column(String(300))
     channel_name_theater_name = Column(String(50))
     
-
+#User inpit to fetch the API data.
+#======================================
 date = '2020-11-18'
 zip_code = '78701'
 line_up_id = 'USA-TX42500-X'
 date_time = '2020-11-18T16:00Z'
-
+#=======================================
+#Calling the API to get the data.
 obj = get_api_data(date,zip_code,line_up_id,date_time)
-    
 local_theaters_df = obj.get_movies_playing_in_local_theaters()
 
-#Need Columns in Database.
+#Cleaning API response for Theaters data.
+
 df_table1 = local_theaters_df[['title','releaseYear','genres','longDescription','showtimes']]
 df_table1['showtimes'] = df_table1['showtimes'].map(lambda x:x[0]['theatre']['name'])
 df_table1.rename(columns={'showtimes':'channel_name_theater_name','longDescription':'description',
                           'releaseYear':'release_year'},inplace=True)
 df_table1['genres'] = df_table1['genres'].map(lambda x:','.join(x) if str(x) != 'nan' else x)
 df_table1 = df_table1[['title','release_year','genres','description','channel_name_theater_name']]
+
+#Cleaning API response for TV data.
 
 line_up_id_df = obj.get_movies_airing_on_TV()
 df_table2 = line_up_id_df[['channels','program']]
@@ -109,43 +110,39 @@ df_table2['description'] = df_table2.apply(lambda x:x['program']['longDescriptio
 df_table2['channel_name_theater_name'] = df_table2['channels'].map(lambda x:','.join(x) if str(x) != 'nan' else x)
     
 df_table2 = df_table2[['title','release_year','genres','description','channel_name_theater_name']]
+#=================================================================================================
+#Loading Cleaned data in mysql data tables.
 
-
-cnx = create_engine('mysql+pymysql://root:Root@localhost:3306/api_data')
+cnx = create_engine('mysql+pymysql://os.environ.get('user'):os.environ.get('password')@localhost:3306/os.environ.get('database_name')')
 Session = sessionmaker(bind=cnx)
 session = Session()
 Base.metadata.create_all(cnx)
 
+#Writing pandas dataframes in table.
 df_table1.to_sql('theater_movies',con=cnx,index=False,if_exists='replace',method='multi')
 df_table2.to_sql('tv_movies',con=cnx,index=False,if_exists='replace',method='multi')
 
-
-
-
+#Reading data from tables into dataframes.
 mysql_cn= MySQLdb.connect(host='localhost', 
-                port=3306,user='root', passwd='Root', 
-                db='api_data')
+                port=3306,user=os.environ.get('user'), passwd=os.environ.get('password'), 
+                db=os.environ.get('database_name'))
 theater_movies = pd.read_sql('select * from theater_movies;', con=mysql_cn) 
 theater_movies['genres'] = theater_movies['genres'].map(lambda x:x.split(',') if x is not None else '')
 theater_movies = theater_movies.explode('genres')
-
+#--------------------------------------------------------------------------------------------------------
 tv_movies = pd.read_sql('select * from tv_movies;', con=mysql_cn) 
 tv_movies['genres'] = tv_movies['genres'].map(lambda x:x.split(',') if x is not None else '')   
 tv_movies = tv_movies.explode('genres')
 
-
+#Combining the data from both the tables to get the groups.
 merged_df = pd.concat([theater_movies,tv_movies])
-
 res_data = merged_df.groupby('genres')['title'].count()
-
 res_data = pd.DataFrame(res_data)
 res_data = res_data.reset_index()
 res_data.rename(columns={'title':'title_count'},inplace=True)
 res_data = res_data.sort_values('title_count',ascending = False).head(5)
 mysql_cn.close()
-
 #=========================================================
+#Printing the data as per question asked.
 #Top 5 genres are:
 print (res_data)
-    
-
